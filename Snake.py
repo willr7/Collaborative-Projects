ab97f0d124bc90970fbd1654b32a7d81c6e0cf9a
lukihu <92463864+lukihu@users.noreply.github.com>
2021-10-13 10:31:53 -0700

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 
+
+
+#####WWWWWWWWWWWWIIIIIIIIIIIIIIIIIIILLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
 WIDTH = 600
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("Snake game")
